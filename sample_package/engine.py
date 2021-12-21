4108748a35e72af5a444433c14f2abcd2b2a75d0
sukim96 <sukim961@gmail.com>
2021-12-21 13:35:31 +0900

    feat: Add num_classes (output classes) to engine

--- sample_package/engine.py
+++ sample_package/engine.py
@@ -5,9 +5,9 @@ from torchvision.models import resnet18
 
 
 class Engine:
-    def __init__(self, device='cuda'):
+    def __init__(self, num_classes=3, device='cuda'):
         self.device = device
-        self.model = resnet18().to(self.device).eval()
+        self.model = resnet18(num_classes).to(self.device).eval()
         self.model.requires_grad_(False)
 
     def forward(self, x):
